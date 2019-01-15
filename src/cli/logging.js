2aa69b3d0792f24852e38bcb4659fef375fd8d43
kawing-ho <kawing-ho@users.noreply.github.com>
2019-01-15 01:37:54 +0000

    indenting

--- src/cli/logging.js
+++ src/cli/logging.js
@@ -19,7 +19,7 @@ export function debug(str: string) {
 export function post_process() {
   let log_location = process.env["YARN_DEBUG_PATH"] || "/tmp/debug.log";
 
-  let results = fs.readFile(log_location, function() {} ).split("\n");
+  let results = fs.readFileSync(log_location, 'utf8').split("\n");
   results = results.filter(String);   // remove empty string
 
 
@@ -27,13 +27,36 @@ export function post_process() {
   results.forEach( function(s, index) {
 
     // change the indenting (conditional)
+    let indent_depth = depth;
+
+    // BEGIN and END of same process should be on same indent
+    if(results[index].match("END") && results[index-1].match("BEGIN")) {
+      indent_depth = depth - 1;
+      depth--;
+    }
+
+    if(results[index].match("BEGIN") && index > 0 && results[index-1].match("END")) {
+      indent_depth = depth + 1;
+      depth++;
+    }
+
+    results[index] = `(${indent_depth-1})\t` + "    ".repeat(indent_depth-1) + s;
+    results[index] = results[index].replace(/\]\t/, "]\t\t");
 
     // increase/decrease indent for next line
+    if(s.match("BEGIN")) {
+      depth++;
+    } else if (s.match("END")) {
+      depth--;
+    } else { throw new Error('Regex mismatch !'); }
+    
+    results[index] = results[index].replace(/\|BEGIN\|/, "").replace("/\>END\</",'');
+    console.error(results[index]);
 
   });
 
   // write output from array
   let out = fs.createWriteStream(log_location);
   out.on('error', function(err) { console.error("Oops, output span error !!!\n") });
-  results.forEach(s => out.write(s));
+  results.forEach(s => out.write(s + "\n"));
 }
